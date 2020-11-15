b8f752bbac8cd2577a9cc0570ca039a9e77303fb
mrxx99 <mrxx99@googlemail.com>
2020-11-15 23:28:31 +0100

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public string A { get; set; }
+        public int B { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Types in this assembly:");
+            foreach (Type t in typeof(Program).Assembly.GetTypes())
+            {
+                Console.WriteLine(t.FullName);
+            }
+
+            var ps = CompiledReflection.GetPropertyNames<Program>();
+
+            foreach (var p in ps)
+            {
+                Console.WriteLine(p);
+            }
+
+            ps = CompiledReflection.GetPropertyNames<TestClass>();
+
+            foreach (var p in ps)
+            {
+                Console.WriteLine(p);
+            }
+
+            var pis = CompiledReflection.GetPropertyInfo<TestClass>();
+
+            var tc = new TestClass { MyProperty = 42, MyProperty2 = "23" };
+
+            foreach (var pi in pis)
+            {
+                Console.WriteLine($"{pi.TypeName} {pi.Name} {pi.GetValue(tc)}");
+            }
+        }
+    }
+
+    public class TestClass
+    {
+        public int MyProperty { get; set; }
+        public string MyProperty2 { get; set; }
+    }
+}
---
+++ SourceGenerator/CompiledReflectionSyntaxReceiver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SourceGenerator
+{
+    public class CompiledReflectionSyntaxReceiver : ISyntaxReceiver
+    {
+        public List<InvocationExpressionSyntax> ExpressionsUsingCompiledReflection { get; } = new();
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is InvocationExpressionSyntax invocationExpression &&
+                invocationExpression.ToFullString().Contains("CompiledReflection"))
+            {
+                ExpressionsUsingCompiledReflection.Add(invocationExpression);
+            }
+        }
+    }
+}
